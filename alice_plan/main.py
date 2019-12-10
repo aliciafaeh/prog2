b8cdf23c3866bb574464032b615de8cb9403fdcd
aliciafaeh <47657878+aliciafaeh@users.noreply.github.com>
2019-12-10 18:31:36 +0100

    added template.html
    for navigation

--- alice_plan/main.py
+++ alice_plan/main.py
@@ -1,14 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template 
 
 app = Flask(__name__)
 
 @app.route("/monatsplan")
 def monatsplan():
-    return "Hello, World!"
+    return render_template("index.html")
 
 @app.route("/todo")
 def todo():
-    return "Hello, Salvador"
+    return render_template("todo.html")
     
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
