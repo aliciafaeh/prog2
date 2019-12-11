94cf23a3660515ba7b83345673747636c356c16e
aliciafaeh <47657878+aliciafaeh@users.noreply.github.com>
2019-12-11 08:36:41 +0100

    added python file
    to add dates

---
+++ alice_plan/pythonstuff/daten.py
@@ -0,0 +1,8 @@
+thema = input("Welches Thema?")
+ort=input("Where?")
+datum=input("Date?")
+zeit=input("Time?")
+
+daten = {'subject': thema, 'where': ort, 'date': datum, 'time': zeit}
+
+print(daten)
