ee7f232dea4f7d5ddb0ebb60def9eea363b11793
aliciafaeh <47657878+aliciafaeh@users.noreply.github.com>
2019-12-11 09:03:15 +0100

    added json file

--- alice_plan/pythonstuff/daten.py
+++ alice_plan/pythonstuff/daten.py
@@ -6,3 +6,44 @@ zeit=input("Time?")
 daten = {'subject': thema, 'where': ort, 'date': datum, 'time': zeit}
 
 print(daten)
+
+import json
+
+def termin_erfassen(subject, where, date, time):
+    
+    json_daten = load_json()
+    alle_termine = json_daten.get("termine", {})
+        
+    termin = {
+        "Subject": thema,
+        "Where": ort,
+        "Date": datum,
+        "Time": zeit
+    }
+        
+    alle_termine[thema] = termin
+        
+    json_daten["termine"] = alle_termine
+
+    save_to_json(json_daten)
+    return json_daten                        #Json-Datei mit neuer Eingabe wird zurückgegeben
+
+def load_json():
+    json_daten = {}
+    try:
+        with open('data/data.json') as open_file:    #Json-Datei öffnen/lesen
+            json_daten = json.load(open_file)
+    
+    except FileNotFoundError:                       #wenn json.datei noch leer ist     
+        print("File not found")
+
+    return json_daten
+
+def save_to_json(daten):
+    with open('data/data.json', "w") as open_file:
+        json.dump(daten, open_file)
+
+
+
+
+# youtube-Tutorial dazu: https://www.youtube.com/watch?v=drord9gbr3Y
