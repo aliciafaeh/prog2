320547ae4cd0d541e872d5b69fcadbd2eaee8e8d
aliciafaeh <47657878+aliciafaeh@users.noreply.github.com>
2020-01-11 11:48:19 +0100

    data.py kommentiert

--- alice_plan/libs/data.py
+++ alice_plan/libs/data.py
@@ -1,12 +1,28 @@
+"""
+Summary:
+
+data.py ist eine Libary für das lesen und schreiben von der json-File
+"""
+
 import json
 
 def load_json():
+	"""
+	Lädt alle Daten von Termine und To Do's aus der json-File
+	
+	Argumente:
+		data/data.json --> Ist der Path zur File
+
+	Returns:
+		Dictionary mit den Daten von "termine" und "todos" wird zurückgegeben. 
+		Wenn File noch leer oder nicht vorhanden ist, wird sozusagen leere json-file wiedergegeben.
+	"""
     json_daten = {}
     try:
-        with open('data/data.json') as open_file:    #Json-Datei öffnen/lesen
+        with open('data/data.json') as open_file:    #Json-File öffnen/lesen
             json_daten = json.load(open_file)
     
-    except FileNotFoundError:                       #wenn json.datei noch leer ist     
+    except FileNotFoundError:                       #wenn data.json noch leer ist     
         json_daten = {
 		  "termine": {
 		    "Work": {
@@ -38,5 +54,12 @@ def load_json():
     return json_daten
 
 def save_to_json(daten):
+	"""
+	Summary
+
+	Argumente:
+		data/data-json ist der Path zur json-File
+		daten (TYPE): Beschreibung
+	"""
     with open('data/data.json', "w", encoding="utf-8") as open_file:
         json.dump(daten, open_file)
