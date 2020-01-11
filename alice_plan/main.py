e3d224bab5c4e8c9d3b4c2646586e3b0ffd4bed2
aliciafaeh <47657878+aliciafaeh@users.noreply.github.com>
2020-01-11 13:08:06 +0100

    fix indent errors

--- alice_plan/main.py
+++ alice_plan/main.py
@@ -1,18 +1,38 @@
 """Summary
 
+Attributes: 
+    app (flask.app.Flask): Der Name meiner App :)
 
 """
 from flask import Flask, render_template, redirect, request, url_for
 from libs import speichern_termine, speichern_todo, data
 
-app = Flask(__name__)
+app = Flask("alice_plan")
 
 @app.route("/")
 def startseite():
-	return render_template('index.html')
+    """
+    Summary:
+        Zeigt die Startseite an, wenn die url/route '/' ist.
+        Auf der Startseite kann man wählen, ob ein Event oder To Do hinzugefügt werden soll.
+    
+
+    Returns:
+        template: Das HTML 'index.html' wird gerendert mit der Auswahlmöglichkeit
+    """
+    return render_template('index.html')
 
 @app.route("/addanevent", methods=['GET', 'POST'])
 def addanevent():
+    """
+    Summary:
+        Hier kann ein Event hinzugefügt werden.
+        Die url/route ist '/addanevent'.
+
+    Returns:
+        template: Das HTML 'eventerfassen' wird gerendert, wo Form ausgefüllt werden kann.
+        redirect: sobald auf 'add' geklickt wird, wird auf den Router 'monatsplananzeige' verlinkt.
+    """
     if request.method == 'POST':
         print(request.form)
         category = request.form['category']
@@ -27,18 +47,40 @@ def addanevent():
 @app.route("/monatsplananzeige")
 @app.route("/monatsplananzeige/<categoryfilter>")
 def monatsplananzeige(categoryfilter=None):
+    """
+    Summary:
+        Hier werden alle Events angezeigt.
+        Die url/route ist '/monatsplananzeige' oder wenn categoryfilter gesetzt: '/monatsplananzeige/<categoryfilter>'.
+    
+    Argumente:
+        categoryfilter: wenn vorhanden, wird er mitgegeben, sonst nicht. --> für filtern von Events.
+
+    Returns:
+        if:
+            template: Das HTML 'monatsplan' wird mit dem categoryfilter gerendert. Es werden nur Termine der Zukunft angezeigt
+        else:
+            template: Das HTML 'monatsplan' wird ohne categoryfilter gerendert. Es werden alle erfassten Termine angezeigt
+    """
     termin_daten = data.load_json()
 
     if categoryfilter:
         termine_from_now = speichern_termine.get_events_from_now(termin_daten['termine'][categoryfilter])
         return render_template("monatsplan.html", daten=termine_from_now, categoryfilter=categoryfilter)
-        #return render_template("monatsplan.html", daten=termin_daten['termine'][categoryfilter], categoryfilter=categoryfilter)
     else:
         return render_template("monatsplan.html", daten=termin_daten)
 
 
 @app.route("/todoerfassen", methods=['GET', 'POST'])
 def todoerfassen():
+    """
+    Summary:
+        Hier kann ein To Do hinzugefügt werden.
+        Die url/route ist '/todoerfassen'.
+
+    Returns:
+        template: Das HTML 'todoerfassen' wird gerendert, wo Form ausgefüllt werden kann.
+        redirect: sobald auf 'add' geklickt wird, wird auf den Router 'todosanzeige' verlinkt.
+    """
     if request.method == 'POST':
         print(request.form)
         was_machen = request.form['was_machen']
@@ -52,6 +94,20 @@ def todoerfassen():
 
 @app.route("/todosanzeige")
 def todosanzeige():
+    """
+    Summary:
+        Hier werden alle Events angezeigt.
+        Die url/route ist '/monatsplananzeige' oder wenn categoryfilter gesetzt: '/monatsplananzeige/<categoryfilter>'.
+    
+    Argumente:
+        categoryfilter: wenn vorhanden, wird er mitgegeben, sonst nicht. --> für filtern von Events.
+
+    Returns:
+        if:
+            template: Das HTML 'monatsplan' wird mit dem categoryfilter gerendert. Es werden nur Termine der Zukunft angezeigt
+        else:
+            template: Das HTML 'monatsplan' wird ohne categoryfilter gerendert. Es werden alle erfassten Termine angezeigt
+    """
     todo_daten = data.load_json()
     return render_template("todosanzeige.html", daten=todo_daten)
 
